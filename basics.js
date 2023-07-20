172f7e9c0f52f7fb2c7ec9fe5749253566ad46c1
saizinlatt29 <saizin.latt.it@gmail.com>
2023-07-20 09:50:37 -0700

    day2

---
+++ basics.js
@@ -0,0 +1,27 @@
+var myName = "Van Luc"
+var MyName = "David"
+
+console.log(myName)
+console.log(MyName)
+
+//semicolons ; are optional
+//single line comment - ctrl+/ (can be to toggled)
+// Test
+//Multi line comment /* */
+
+//Declaring Identifers
+var session1, session2 ;
+//var 3seesion not allow 
+//$ and _ can be used as special chars
+var $session_2 ;
+var _sess ;
+
+//It is untype language - assign any data an identifier
+
+var number_of_session = true //5/"Five"
+console.log(number_of_session)
+
+//It is dynamic casting enable 
+
+number_of_session =1000;
+console.log(number_of_session)
---
+++ datatype.js
@@ -0,0 +1,26 @@
+//Datatype int, string ,char ,bool, etc
+//for Java script
+//String , Number, Bollen,Undefined , Null, Object
+
+//in ES6 above we can have Symbol as data
+
+var checkDataType = 1.256 //
+console.log("Data type is - "+ typeof checkDataType)
+checkDataType = 1256
+console.log("Data type is - "+ typeof checkDataType)
+checkDataType = "Ryan"
+console.log("Data type is - "+ typeof checkDataType)
+checkDataType = true
+console.log("Data type is - "+ typeof checkDataType)
+checkDataType = undefined //is avalid data type
+console.log("Data type is - "+ typeof checkDataType)
+checkDataType = null //is a valid for object type as null is just is nothing 
+console.log("Data type is - "+ typeof checkDataType)
+checkDataType ={}
+console.log("Data type is - "+ typeof checkDataType)
+checkDataType ={
+    name :"Arit",
+    session:"Mernstack"
+}
+console.log("Data type is - "+ typeof checkDataType)
+
